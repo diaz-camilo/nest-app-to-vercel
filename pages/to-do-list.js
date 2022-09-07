a69c10e8edf6460a711986249964f82d96c09ce2
diaz-camilo <diaz.camilo@icloud.com>
2022-09-07 23:49:27 +1000

    added input and button, button logs message when clicked

--- pages/to-do-list.js
+++ pages/to-do-list.js
@@ -3,6 +3,11 @@ import Head from 'next/head';
 import utilStyles from '../styles/utils.module.css';
 
 export default function ToDoList() {
+  function handleOnClick(event) {
+    console.log('clicked the button');
+
+    event.preventDefault(); // this is needed to stop the page refresh
+  }
   return (
     <Layout>
       <Head>
@@ -10,7 +15,19 @@ export default function ToDoList() {
       </Head>
       <article>
         <h1 className={utilStyles.headingXl}>To Do List</h1>
-        <div className={utilStyles.todoListContainer}></div>
+        <div className={utilStyles.todoListContainer}>
+          <div>
+            <form>
+              <input type={'text'}></input>
+              {/* HTML-like components in react have onClick event handlers. 
+              you can pass a function to this event handler to handle the event.
+              here for example we are telling react that every time the user click
+              this button, it should call the handle click function
+              https://reactjs.org/docs/forms.html */}
+              <button onClick={handleOnClick}>add item</button>
+            </form>
+          </div>
+        </div>
       </article>
     </Layout>
   );
